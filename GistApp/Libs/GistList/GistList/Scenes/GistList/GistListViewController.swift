455fa49cf7976e0117155d56ab15a1a9a2cb7f10
joaofloresdev <j199818@dac.unicamp.br>
2024-05-23 16:14:19 -0300

    Tela de detalhes sendo apresentada a partir do app principal

--- GistApp/Libs/GistList/GistList/Scenes/GistList/GistListViewController.swift
+++ GistApp/Libs/GistList/GistList/Scenes/GistList/GistListViewController.swift
@@ -4,3 +4,20 @@
 //
 //  Created by Joao Victor Flores da Costa on 23/05/24.
 //
+
+import UIKit
+
+public class GistListViewController: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
