7d4045a1b5634935c5fb8e2646bb6bc76cc62da6
joaofloresdev <j199818@dac.unicamp.br>
2024-05-25 00:10:04 -0300

    Atualização de target minimo e testes

--- GistApp/Libs/GistList/GistListTests/Scenes/GistList/GistListViewControllerTests.swift
+++ GistApp/Libs/GistList/GistListTests/Scenes/GistList/GistListViewControllerTests.swift
@@ -1,8 +1,58 @@
-//
-//  GistListViewControllerTests.swift
-//  GistListTests
-//
-//  Created by Joao Victor Flores da Costa on 25/05/24.
-//
+import XCTest
+@testable import GistApp
 
-import Foundation
+class GistListViewControllerTests: XCTestCase {
+    var viewController: GistListViewController!
+    var mockInteractor: MockGistListInteractor!
+    
+    override func setUp() {
+        super.setUp()
+        mockInteractor = MockGistListInteractor()
+        viewController = GistListViewController(interactor: mockInteractor)
+        viewController.loadViewIfNeeded()
+    }
+    
+    override func tearDown() {
+        viewController = nil
+        mockInteractor = nil
+        super.tearDown()
+    }
+    
+    func testViewDidLoad() {
+        XCTAssertTrue(mockInteractor.populateGistsCalled, "populateGists() should be called when view is loaded")
+    }
+    
+    func testDisplayGists() {
+        let gists = [GistCellModel(userName: "User1", userImageUrl: nil, filesAmount: "5 files")]
+        viewController.displayGists(data: gists)
+        
+        XCTAssertEqual(viewController.tableView.numberOfRows(inSection: 0), gists.count)
+        XCTAssertFalse(viewController.loadingView.isHidden)
+    }
+    
+    func testDisplayError() {
+        let errorModel = ErrorModel(title: "Error", subtitle: "Something went wrong", buttonText: "Retry")
+        viewController.displayError(model: errorModel)
+        
+        XCTAssertFalse(viewController.loadingView.isHidden)
+        XCTAssertNotNil(viewController.presentedViewController as? UIAlertController)
+    }
+    
+    func testDisplayLoading() {
+        viewController.displayLoading()
+        XCTAssertFalse(viewController.loadingView.isHidden)
+    }
+}
+
+class MockGistListInteractor: GistListInteractorProtocol {
+    var populateGistsCalled = false
+    var gistSelectedCalled = false
+    
+    func populateGists() {
+        populateGistsCalled = true
+    }
+    
+    func gistSelected(index: Int) {
+        gistSelectedCalled = true
+    }
+}
