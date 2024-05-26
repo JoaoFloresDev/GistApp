e2bb9b852399836168d73395525f3548ee79ee2d
joaofloresdev <j199818@dac.unicamp.br>
2024-05-26 06:29:42 -0300

    Correção de ingração entre módulos

--- GistApp/Libs/GistList/GistListTests/GistListTests.swift
+++ GistApp/Libs/GistList/GistListTests/GistListTests.swift
@@ -8,30 +8,116 @@
 import XCTest
 @testable import GistList
 
-final class GistListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        Abc()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class GistListInteractorMock: GistListInteractorProtocol {
+    var populateGistsCallsCount = 0
+    func populateGists() {
+        populateGistsCallsCount += 1
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    var gistSelectedCallsCount = 0
+    var gistSelectedValue: Int?
+    func gistSelected(index: Int) {
+        populateGistsCallsCount += 1
+        gistSelectedValue = index
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+final class GistListViewsTests: XCTestCase {
+    func makeSut() -> GistListViewController {
+        GistListViewController(interactor: GistListInteractorMock())
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_whenGistsAreDisplayed_thenTableViewHasCorrectNumberOfCells() {
+        let sut = makeSut()
+        sut.displayGists(data: [
+            .init(userName: nil, userImageUrl: nil, filesAmount: nil),
+            .init(userName: nil, userImageUrl: nil, filesAmount: nil)
+        ])
+        sut.viewDidLoad()
+        
+        let mirror = Mirror(reflecting: sut)
+        var foundTableView: UITableView? = nil
+        
+        for child in mirror.children {
+            if let tableView = child.value as? UITableView {
+                foundTableView = tableView
+                break
+            }
         }
+        
+        XCTAssertNotNil(foundTableView)
+        let numberOfCells = foundTableView?.numberOfRows(inSection: 0)
+        XCTAssertEqual(numberOfCells, 2)
     }
 
+    func test_whenLoadingViewIsDisplayed_thenItIsNotHidden() {
+        let controller = makeSut()
+        controller.viewDidLoad()
+        
+        let mirror = Mirror(reflecting: controller)
+        var foundLoadingView: UIView? = nil
+        
+        for child in mirror.children {
+            if let view = child.value as? UIView {
+                foundLoadingView = view
+                break
+            }
+        }
+        
+        guard let loadingView = foundLoadingView else {
+            XCTFail("loadingView should be a property of GistListViewController")
+            return
+        }
+        
+        controller.displayLoading()
+        XCTAssertFalse(loadingView.isHidden)
+    }
+
+    func test_whenViewDidLoad_thenLoadingViewIsInitiallyHidden() {
+        let controller = makeSut()
+        controller.viewDidLoad()
+        
+        let mirror = Mirror(reflecting: controller)
+        var foundLoadingView: UIView? = nil
+        
+        for child in mirror.children {
+            if let view = child.value as? UIView {
+                foundLoadingView = view
+                break
+            }
+        }
+        
+        guard let loadingView = foundLoadingView else {
+            XCTFail("loadingView should be a property of GistListViewController")
+            return
+        }
+        XCTAssertTrue(loadingView.isHidden)
+    }
+
+    func test_whenGistCellIsConfigured_thenNameAndFilesLabelsHaveCorrectText() {
+        let model = GistCellModel(userName: "Test User", userImageUrl: nil, filesAmount: "5 files")
+        let cell = GistCell(model: model)
+        
+        let mirror = Mirror(reflecting: cell)
+        var foundNameLabel: UILabel? = nil
+        var foundFilesLabel: UILabel? = nil
+        
+        for child in mirror.children {
+            if let nameLabel = child.value as? UILabel,
+               "$__lazy_storage_$_nameLabel" == child.label {
+                foundNameLabel = nameLabel
+            }
+            
+            if let filesLabel = child.value as? UILabel,
+               "$__lazy_storage_$_filesLabel" == child.label {
+                foundFilesLabel = filesLabel
+            }
+        }
+        
+        XCTAssertNotNil(foundNameLabel)
+        XCTAssertEqual(foundNameLabel?.text, "Test User")
+        
+        XCTAssertNotNil(foundFilesLabel)
+        XCTAssertEqual(foundFilesLabel?.text, "5 files")
+    }
 }
